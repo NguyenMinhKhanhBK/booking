f9d2fa123cc9289dc0a58e674a3b6a39d009dee7
Khanh Nguyen <khanhnguyen@axon.com>
2022-03-25 10:42:56 +0700

    Add user migrations

---
+++ migrations/20220325033916_add_user_to_users_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+(E'Khanh',E'Nguyen',E'admin@admin.com',E'$2a$12$K31I59B2VpTqpmSxwYI9HO.h.9u5nN6XfugjRogZYDi7mohumgOc2',3,E'2022-01-01 00:00:00',E'2022-01-01 00:00:00');
